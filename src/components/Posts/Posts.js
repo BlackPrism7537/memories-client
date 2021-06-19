7cf0f75a6d75d6eca24556eaddd8352e7343b44c
ZachBarcoe <z.barcoe@gmail.com>
2021-06-19 15:58:53 +0000

    posts display

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,17 +1,29 @@
 import React from "react";
 import Post from "./Post/Post";
+import { useSelector } from "react-redux";
 
 import useStyles from "./styles";
+import { CircularProgress, Grid, Typography } from "@material-ui/core";
 
 const Posts = () => {
     const classes = useStyles();
+    const posts = useSelector((state) => state.posts);
 
-    return (
-        <>
-            <Post />
-            <Post />
-            <Post />
-        </>
+    return !posts.length ? (
+        <CircularProgress />
+    ) : (
+        <Grid
+            container
+            className={classes.container}
+            alignItems="stretch"
+            spacing={2}
+        >
+            {posts.map((post) => (
+                <Grid item xs={12} sm={6} key={post._id}>
+                    <Post post={post} />
+                </Grid>
+            ))}
+        </Grid>
     );
 };
 
